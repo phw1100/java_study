4069e0e59fd75d5d5487c2151c5a7b85748d90e6
phw1100 <74524170+phw1100@users.noreply.github.com>
2023-11-16 17:11:31 +0900

    ch_1

---
+++ ch_1/src/Runstatement.java
@@ -0,0 +1,8 @@
+public class Runstatement {
+    public static void main(String[] args) throws Exception {
+        int x = 1;
+        int y = 2;
+        int result = x+y;
+        System.out.println(result);
+    }
+}
