dfab2a49f20f1208c9059ecdf60d7762e163542f
phw1100 <74524170+phw1100@users.noreply.github.com>
2023-11-19 21:56:24 +0900

    ch_3

---
+++ ch_3/src/DenyLogic.java
@@ -0,0 +1,12 @@
+public class DenyLogic {
+    public static void main(String[] args) {
+        boolean play = true;
+        System.out.println(play);
+
+        play = !play;
+        System.out.println(play);
+
+        play = !play;
+        System.out.println(play);
+    }
+}
