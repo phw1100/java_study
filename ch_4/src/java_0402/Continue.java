460da482f4eb5f52f12761e7e235879125e26b91
phw1100 <74524170+phw1100@users.noreply.github.com>
2023-11-22 19:46:03 +0900

    ch_4

---
+++ ch_4/src/java_0402/Continue.java
@@ -0,0 +1,12 @@
+package java_0402;
+
+public class Continue {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            if(i%2!=0){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ ch_4/src/java_0402/WhilePrint.java
@@ -0,0 +1,11 @@
+package java_0402;
+
+public class WhilePrint {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
